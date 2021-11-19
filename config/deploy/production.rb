9c7f1c92f0fb03a2fe4d2e63f3c8b2776bc54988
HobbesG <calving159@gmail.com>
2021-11-19 11:36:08 -0500

    commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,5 @@
 # server-based syntax
 # ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
@@ -12,11 +10,6 @@ server '161.35.128.233', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
-
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
 
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
